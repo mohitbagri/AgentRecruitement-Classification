270308cc5f1bb8e5a4a734bd5cca4957203e0d45
Mohit Bagri <mohitbagri96@gmail.com>
2019-08-25 20:13:45 -0400

    Update classification.py

--- classification.py
+++ classification.py
@@ -3,36 +3,34 @@ import pandas as pd
 from datetime import datetime
 from dateutil import parser
 import scipy.stats
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import RandomizedSearchCV
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
-# Reading data from CSV file
+# Read data from CSV file
 df=pd.read_csv("C:/Users/MOHIT/Desktop/AV/Train.csv")
 df1=pd.read_csv("C:/Users/MOHIT/Desktop/AV/Test.csv")
 data = pd.concat([df,df1], ignore_index=True)
 df=data.copy()
 
-# Replacing missing values in the dataset
+# Replacing null values in dataset
 for i in df.columns:
     df[i].fillna(df[i].mode()[0],inplace=True)
 
 # Data parsing and processing
-df['new_Manager_Grade']=df['Manager_Grade'].fillna(np.mean(df['Manager_Grade']))
-df['Manager_Grade']=pow((df['Manager_Grade']),0.333)
-
 df.loc[df['Manager_Num_Coded']>4,'Manager_Num_Coded']=4
 df['Manager_Coded']=df['Manager_Num_Coded'].fillna(df['Manager_Num_Coded'].mode()[0])
+
 
 df.loc[df['Manager_Business']<0,'Manager_Business']=np.abs(df['Manager_Business'])
 df['Manager_Business']=df['Manager_Business'].fillna(np.mean(df['Manager_Business']))
 j=0
 for i in df['Manager_Business']:
     if i!=0:
-         df.loc[df.index[j], 'Manager_Business'] = np.log(i)
+        df.loc[df.index[j], 'Manager_Business'] = np.log(i)
     else:
         df.loc[df.index[j], 'Manager_Business'] = i
     j=j+1
@@ -44,13 +42,6 @@ df.loc[(df['Manager_Business'] > 12) & (df['Manager_Business'] < 13), ['Manager_
 df.loc[(df['Manager_Business'] > 13) & (df['Manager_Business'] < 14), ['Manager_Business']]=13
 df.loc[(df['Manager_Business'] > 14) , ['Manager_Business']]=14
 
-df['Manager_Num_Products']=df['Manager_Num_Products'].fillna(np.mean(df['Manager_Num_Products']))
-df['Manager_Num_Products']=pow((df['Manager_Num_Products']),0.3333)
-
-df['Manager_Business2']=pow((df['Manager_Business2']),0.3333)
-df['Manager_Num_Products2']=pow((df['Manager_Num_Products2']),0.3333)
-
-df['Application_Receipt_Date'] = df['Application_Receipt_Date'].astype('datetime64[ns]')
 j=0
 for i in df['Application_Receipt_Date']:
     df.loc[df.index[j],'Application_Receipt_Date'] = parser.parse(i)
@@ -106,14 +97,12 @@ for i in df['Manager_DoB']:
     df.loc[df.index[j],'Manager_DoB'] = parser.parse(i)
     j=j+1
 
-# Creating a shallow copy of the original dataframe, so that the new temporary dataframe can be manipulated and given as input to the classifiers
+#Creating a shallow copy of the original dataframe, so that the new temporary dataframe can be manipulated and given as input to the classifiers
 df2=df.copy()
 drop_columns=['ID']
 df2.drop(drop_columns,axis=1,inplace=True)
-
-# Train and test data splitting
-Uncorrelated_Features=['Application_Receipt_Date','Applicant_BirthDate','Manager_DOJ','Manager_DoB']
-df2=df2.drop(Uncorrelated_Features, axis=1)
+Removed_Features=['Application_Receipt_Date','Applicant_BirthDate','Manager_DOJ','Manager_DoB']
+df2=df2.drop(Removed_Features, axis=1)
 y=data['Business_Sourced'].dropna()
 x=df2
 x=x.drop('Business_Sourced',axis=1)
@@ -123,10 +112,9 @@ IDs=df['ID']
 IDs=np.array(IDs[9527:])
 x_train, x_test,y_train,y_test = train_test_split(x1,y, test_size=0.25, random_state=42)
 
-# Creating the Random Forest Classifier with parameters obtained by hyper parameter tuning
 RF = RandomForestClassifier(n_estimators= 1000, min_samples_split=10, min_samples_leaf=2,max_features='sqrt',max_depth= 10, bootstrap= True)
 
-# Hyper Parameter tuning of the Random Forest
+# Hyper parameter tuning of the Random Forest classifier
 '''
 # Number of trees in random forest
 n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
@@ -152,23 +140,22 @@ random_grid = {'n_estimators': n_estimators,
 rf_random = RandomizedSearchCV(estimator = logisticRegr, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = 1)
 rf_random.fit(x_train, y_train)
 print(rf_random.best_params_)
+
 '''
 
-# Fitting the Random Forest classifier
+# Fitting the model and predicting test data results
 RF.fit(x_train, y_train)
 prediction = RF.predict(x_test)
-
-# Classifying the test data
 score=RF.score(x_test,y_test)
 predictions2 = RF.predict_proba(x_test)
 final_predicion=RF.predict(x_validation)
 final_predicion=np.array(final_predicion)
-
 final_df=pd.DataFrame(final_predicion,index=IDs,columns=['Business_Sourced'])
 final_df['ID'] = final_df.index
 final_col_names=['ID','Business_Sourced']
 final_df=final_df[final_col_names]
 final_df.reset_index(drop=True,inplace=True)
 
-# Writing test data results to output CSV file
+# Writing the results to CSV file
 final_df.to_csv('C:/Users/MOHIT/Desktop/AV/Classification_Soln.csv', encoding='utf-8', index=False)
+
